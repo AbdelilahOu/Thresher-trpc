780670d6e33daa42bdd8a26f557cc6493717df7c
Abdelilah_OU <gxgamer710@gmail.com>
2023-01-21 18:42:27 +0000

    create all routes

---
+++ src/server/routes/productRoute.ts
@@ -0,0 +1,62 @@
+import {
+  createClient,
+  getAllClients,
+  getClient,
+  updateClient,
+} from "../database/repository/clientRepo";
+import { router, procedure } from "../trpc/index";
+import { z } from "zod";
+
+export const clientRoute = router({
+  getAll: procedure.query(() => {
+    return getAllClients();
+  }),
+  findById: procedure.input(z.number()).query(({ input }) => {
+    return getClient(input);
+  }),
+  createOne: procedure
+    .input(
+      z.object({
+        name: z
+          .string({
+            required_error: "name is required",
+          })
+          .min(1),
+        email: z.string().optional(),
+        phone: z.string().optional(),
+        addresse: z.string().optional(),
+      })
+    )
+    .mutation(({ input }) => {
+      return createClient(input);
+    }),
+  updateOne: procedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string().optional(),
+        email: z.string().optional(),
+        phone: z.string().optional(),
+        addresse: z.string().optional(),
+      })
+    )
+    .mutation(({ input }) => {
+      const { id, name, email, phone, addresse } = input;
+      return updateClient({
+        id: id,
+        data: {
+          name,
+          email,
+          phone,
+          addresse,
+        },
+      });
+    }),
+});
+
+// export interface newClientT {
+//   name: string;
+//   email?: string;
+//   phone?: string;
+//   addresse?: string;
+// }
